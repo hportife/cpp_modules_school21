ae573345f04b69b08df11d29a90f2f7f8e0e4406
hportife <hportife@student.21-school.ru>
2022-01-30 12:29:40 +0300

    create & end ex02

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string str("HI THIS IS BRAIN");
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "String address: " << &str << std::endl;
+	std::cout << "Pointer address: " << stringPTR << std::endl;
+	std::cout << "Reference address: " << &stringREF << std::endl;
+
+	std::cout << "String value: " << str << std::endl;
+	std::cout << "Pointer value: " << *stringPTR << std::endl;
+	std::cout << "Reference value: " << stringREF << std::endl;
+}
